bb10c2568e30fbeb75530ef6b8eb32e2b3cfcecb
Vyacheslav Frolov <algebris@gmail.com>
2017-09-13 01:30:46 +0600

    separating module
    MINT-441 - Separating Module
    - clean-ups
    - removing warnings

---
+++ models/blockModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+/** @model blockModel
+ *  @description block model - represents a block in eth
+ */
+const Block = new mongoose.Schema({
+  block: {type: Number},
+  network: {type: String},
+  created: {type: Date, required: true, default: Date.now},
+
+});
+
+module.exports = mongoose.model('EthBlock', Block);
