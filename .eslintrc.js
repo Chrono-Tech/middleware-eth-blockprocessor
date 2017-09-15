7f56065f49a68a31b4835a3f06dc9f4643b23f3e
Vyacheslav Frolov <algebris@gmail.com>
2017-09-15 18:35:20 +0700

    update linter
    MINT-441
    - updated eslinter
    - fixed .gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,13 @@ module.exports = {
     "node": true,
     "es6": true
   },
-  "extends": ["eslint:recommended"],
+  "extends": ['plugin:chronobank/recommended'],
   "rules": {
-    "indent": [
-      "error", 2, {
-        "SwitchCase": 1
-      }
-    ],
     "quotes": ["error", "single"],
-    "camelcase": ["error", {properties: "always"}],
     "semi": ["error", "always"],
     "no-console": 1,
     "no-unused-vars": 1,
-    "no-empty": ["error", {"allowEmptyCatch": true}]
+    "no-empty": ["error", { "allowEmptyCatch": true }]
   },
   "parserOptions": {
     "ecmaVersion": 8
