a5970dda003aea4929319dd284419d106edd4fa9
zyevegor <zyev.egor@gmail.com>
2018-02-14 14:05:18 +0300

    https://chronobank.atlassian.net/browse/MD-174 - middleware eth - tx caching

--- index.js
+++ index.js
@@ -11,16 +11,14 @@ mongoose.Promise = Promise;
 mongoose.connect(config.mongo.data.uri, {useMongoClient: true});
 mongoose.accounts = mongoose.createConnection(config.mongo.accounts.uri, {useMongoClient: true});
 
-const blockModel = require('./models/blockModel'),
-  _ = require('lodash'),
+const _ = require('lodash'),
+  BlockCacheService = require('./services/blockCacheService'),
   bunyan = require('bunyan'),
   Web3 = require('web3'),
-  web3Errors = require('web3/lib/web3/errors'),
   net = require('net'),
   amqp = require('amqplib'),
   log = bunyan.createLogger({name: 'app'}),
-  filterTxsByAccountService = require('./services/filterTxsByAccountService'),
-  blockProcessService = require('./services/blockProcessService');
+  filterTxsByAccountService = require('./services/filterTxsByAccountService');
 
 [mongoose.accounts, mongoose.connection].forEach(connection =>
   connection.on('disconnected', function () {
@@ -31,27 +29,10 @@ const blockModel = require('./models/blockModel'),
 
 const init = async () => {
 
-  log.info('indexing...');
-
-  await new Promise((res, rej) =>
-    blockModel.on('index', error =>
-      error ? rej(error) : res()
-    )
-  ).catch(err => {
-    log.info(`index error: ${err}`);
-    return process.exit(0);
-  });
-
-  log.info('indexation completed!');
-
-  const currentBlocks = await blockModel.find({network: config.web3.network}).sort('-number').limit(config.consensus.lastBlocksValidateAmount);
-  let currentBlock = _.chain(currentBlocks).get('0.number', 0).add(0).value();
-  log.info(`search from block:${currentBlock} for network:${config.web3.network}`);
-
-  let lastBlocks = _.chain(currentBlocks).map(block => block.hash).compact().reverse().value();
   const provider = new Web3.providers.IpcProvider(config.web3.uri, net);
   const web3 = new Web3();
   web3.setProvider(provider);
+  const blockCacheService = new BlockCacheService(web3);
 
   web3.currentProvider.connection.on('end', () => {
     log.error('ipc process has finished!');
@@ -73,8 +54,24 @@ const init = async () => {
 
   await channel.assertExchange('events', 'topic', {durable: false});
 
+  blockCacheService.events.on('block', async block=>{
+    const filteredTxs = await filterTxsByAccountService(block.transactions);
+
+    for (let tx of filteredTxs) {
+      let addresses = _.chain([tx.to, tx.from])
+        .union(tx.logs.map(log => log.address))
+        .uniq()
+        .value();
+
+      for (let address of addresses)
+        await channel.publish('events', `${config.rabbit.serviceName}_transaction.${address}`, new Buffer(JSON.stringify(tx)));
+    }
+  });
+
+  await blockCacheService.startSync();
+
   web3.eth.filter('pending').watch(async (err, result) => {
-    if (err)
+    if (err || !await blockCacheService.isSynced())
       return;
 
     let tx = await Promise.promisify(web3.eth.getTransaction)(result);
@@ -101,66 +98,6 @@ const init = async () => {
     }
 
   });
-
-  /**
-   * Recursive routine for processing incoming blocks.
-   * @return {undefined}
-   */
-  let processBlock = async () => {
-    try {
-      const data = await Promise.resolve(blockProcessService(currentBlock, web3, lastBlocks)).timeout(20000);
-
-      for (let tx of data) {
-        let addresses = _.chain([tx.to, tx.from])
-          .union(tx.logs.map(log => log.address))
-          .uniq()
-          .value();
-
-        for (let address of addresses)
-          await channel.publish('events', `${config.rabbit.serviceName}_transaction.${address}`, new Buffer(JSON.stringify(tx)));
-      }
-
-      await blockModel.findOneAndUpdate({
-          number: data.block.number
-        },
-        _.merge({network: config.web3.network}, data.block),
-        {upsert: true}
-      );
-
-      _.pullAt(lastBlocks, 0);
-      lastBlocks.push(data.block.hash);
-      currentBlock++;
-      processBlock();
-    } catch (err) {
-
-      if (err instanceof Promise.TimeoutError)
-        return processBlock();
-
-      if (_.has(err, 'cause') && err.toString() === web3Errors.InvalidConnection('on IPC').toString())
-        return process.exit(-1);
-
-      if (_.get(err, 'code') === 0) {
-        log.info(`await for next block ${currentBlock}`);
-        return setTimeout(processBlock, 10000);
-      }
-
-      if (_.get(err, 'code') === 1) {
-
-        let lastCheckpointBlock = await blockModel.findOne({hash: lastBlocks[0]});
-        log.info(`wrong sync state!, rollback to ${lastCheckpointBlock.number - 1} block`);
-        await blockModel.remove({hash: {$in: lastBlocks}});
-        const currentBlocks = await blockModel.find({network: config.web3.network}).sort('-number').limit(config.consensus.lastBlocksValidateAmount);
-        lastBlocks = _.chain(currentBlocks).map(block => block.hash).reverse().value();
-        currentBlock = lastCheckpointBlock - 1;
-        return processBlock();
-      }
-
-      currentBlock++;
-      processBlock();
-    }
-  };
-
-  processBlock();
 
 };
 
