b3584dc2a17334bc8931b73fa97b91c3881e584b
kirya <cloudkserg11@gmail.com>
2018-03-12 09:11:12 +0300

    https://chronobank.atlassian.net/browse/MD-215 add docjs comments to services MasterNode.js

--- services/MasterNode.js
+++ services/MasterNode.js
@@ -8,9 +8,34 @@ const MASTER_SYNC_TIMEOUT = 2000;
 const MASTER_PREV_SYNC_TIMEOUT = 10000; 
 
 const EXCHANGE_NAME='master_events';
-  
+
+/**
+ * @class MasterNode
+ * 
+ * Class, that helps with clasterization process with mode [one master and many slaves]
+ * 
+ * when run this class, though rabbitmq process
+ * find exist master and remember ourself as slave
+ * 
+ * when prev master is died or just no response by rabbitmq
+ * election for master run away and new Master is done.
+ * may be is this process
+ *
+ * 
+ * 
+ */
 class MasterNode {
 
+    /**
+     * 
+     * Constructor, that only create main variables in class
+     * not done anything work
+     * 
+     * @param Channel [from amqplib] _channel Channel, through send and response messages 
+     * @param function [logFunction=() => {}] function for log main messages
+     * 
+     * @memberOf MasterNode
+     */
     constructor(_channel, logFunction = () => {}) {
         const myid = uniqid();
         
@@ -87,6 +112,14 @@ class MasterNode {
         
     }
 
+    /**
+     * 
+     * Async start function
+     * in this function process subscribe on main events in rabbitmq, connected to elections
+     * and through MASTER_UPDATE_TIMEOUT run periodic checkMasterProcess
+     * 
+     * @memberOf MasterNode
+     */
     async start() {
         await this.channel.assertExchange(EXCHANGE_NAME, 'direct', {autoDelete: true});
         this._onSetMasterEvent(this._setNewMasterFromEvent);
@@ -96,6 +129,17 @@ class MasterNode {
         setTimeout(this._updateMaster, MASTER_UPDATE_TIMEOUT);
     }
 
+    /**
+     * 
+     * Async isSyncMaster function
+     * check is Master this process, or not
+     * 
+     * and is not master run forced check exists master in network or not
+     * 
+     * @returns boolean
+     * 
+     * @memberOf MasterNode
+     */
     async isSyncMaster() {
         if (!this._isMasterSynced) {
             await Promise.delay(MASTER_SYNC_TIMEOUT);
