5abdd36b59126887a38d3bb1bde76301a9a8b1a5
kirya <cloudkserg11@gmail.com>
2018-03-14 11:58:11 +0300

    https://chronobank.atlassian.net/projects/MD/issues/MD-215 - eth middleware - change in blockprocessor index.js from eth.watch for pending to blockCacheService on eventsEmitter

--- services/MasterNode.js
+++ services/MasterNode.js
@@ -1,6 +1,5 @@
-const amqp = require('amqplib'),
-    config = require('../config'),
-    Promise = require('bluebird');
+const  config = require('../config'),
+  Promise = require('bluebird');
 const uniqid = require('uniqid');
 
 const MASTER_UPDATE_TIMEOUT = 2000;
@@ -27,7 +26,7 @@ const EXCHANGE_NAME='master_events';
  */
 class MasterNode {
 
-    /**
+  /**
      * 
      * Constructor, that only create main variables in class
      * not done anything work
@@ -37,88 +36,88 @@ class MasterNode {
      * 
      * @memberOf MasterNode
      */
-    constructor(_channel, logFunction = () => {}) {
-        const myid = uniqid(),
-            findMasterQueue = `${config.rabbit.serviceName}_findMaster_${myid}`,
-            findMasterRoute = `${config.rabbit.serviceName}_findMaster`,
-            setMasterQueue = `${config.rabbit.serviceName}_setMaster_${myid}`,
-            setMasterRoute = `${config.rabbit.serviceName}_setMaster`;
+  constructor (_channel, logFunction = () => {}) {
+    const myid = uniqid(),
+      findMasterQueue = `${config.rabbit.serviceName}_findMaster_${myid}`,
+      findMasterRoute = `${config.rabbit.serviceName}_findMaster`,
+      setMasterQueue = `${config.rabbit.serviceName}_setMaster_${myid}`,
+      setMasterRoute = `${config.rabbit.serviceName}_setMaster`;
 
         
-        this.channel = _channel;
-        this._isMaster = false;
-        this._isMasterSynced = true;
-        this._currentMaster = undefined;
-        this._prevSyncTime = Date.now();
+    this.channel = _channel;
+    this._isMaster = false;
+    this._isMasterSynced = true;
+    this._currentMaster = undefined;
+    this._prevSyncTime = Date.now();
 
-        const notFindMaster = () => this._currentMaster === undefined;
-        const log = (msg) => logFunction(`master-node#${myid}: ${msg}`);
+    const notFindMaster = () => this._currentMaster === undefined;
+    const log = (msg) => logFunction(`master-node#${myid}: ${msg}`);
           
 
 
-        const sendFindMasterEvent= async() => {
-            await this.channel.publish('master_events', findMasterRoute, new Buffer(myid));
-        }
+    const sendFindMasterEvent= async () => {
+      await this.channel.publish('master_events', findMasterRoute, new Buffer(myid));
+    };
 
-        this._isPrevSyncFreeze = () => {
-            return (Date.now() - this._prevSyncTime) > MASTER_PREV_SYNC_TIMEOUT;
-        }
+    this._isPrevSyncFreeze = () => {
+      return (Date.now() - this._prevSyncTime) > MASTER_PREV_SYNC_TIMEOUT;
+    };
 
-        this._setNewMasterFromEvent = (masterId) => {
-            this._currentMaster = masterId;
-            this._isMaster = (this._currentMaster === myid); 
-        };
+    this._setNewMasterFromEvent = (masterId) => {
+      this._currentMaster = masterId;
+      this._isMaster = (this._currentMaster === myid); 
+    };
     
-        this._sendSetMasterEvent = async() => {
-            await this.channel.publish(`master_events`, setMasterRoute, new Buffer(myid));
-        };
-        this._onFindMasterEvent = async (handler) => {
-            await this.channel.assertQueue(findMasterQueue);
-            await this.channel.bindQueue(findMasterQueue, EXCHANGE_NAME, findMasterRoute, {autoDelete: true});
+    this._sendSetMasterEvent = async () => {
+      await this.channel.publish('master_events', setMasterRoute, new Buffer(myid));
+    };
+    this._onFindMasterEvent = async (handler) => {
+      await this.channel.assertQueue(findMasterQueue);
+      await this.channel.bindQueue(findMasterQueue, EXCHANGE_NAME, findMasterRoute, {autoDelete: true});
         
-            this.channel.consume(findMasterQueue, async (message) => {
-            await handler(message.content.toString());
-                this.channel.ack(message);    
-            });
-        };
+      this.channel.consume(findMasterQueue, async (message) => {
+        await handler(message.content.toString());
+        this.channel.ack(message);    
+      });
+    };
 
-        this._onSetMasterEvent = async (handler) => {
-            await this.channel.assertQueue(setMasterQueue);
-            await this.channel.bindQueue(setMasterQueue, EXCHANGE_NAME, setMasterRoute, {autoDelete: true});
+    this._onSetMasterEvent = async (handler) => {
+      await this.channel.assertQueue(setMasterQueue);
+      await this.channel.bindQueue(setMasterQueue, EXCHANGE_NAME, setMasterRoute, {autoDelete: true});
         
-            this.channel.consume(setMasterQueue, async(message) => {
-                handler(message.content.toString());
-                this.channel.ack(message);
-            });
-        };
+      this.channel.consume(setMasterQueue, async (message) => {
+        handler(message.content.toString());
+        this.channel.ack(message);
+      });
+    };
 
-        this._updateMaster = async () => {
-            this._prevSyncTime = Date.now();            
-            this._isMasterSynced = false;
-            this._currentMaster = undefined;
-            log('syncing: started');
+    this._updateMaster = async () => {
+      this._prevSyncTime = Date.now();            
+      this._isMasterSynced = false;
+      this._currentMaster = undefined;
+      log('syncing: started');
         
-            await sendFindMasterEvent();
-            log('syncing: finded');
-            await Promise.delay(MASTER_FIND_TIMEOUT);
+      await sendFindMasterEvent();
+      log('syncing: finded');
+      await Promise.delay(MASTER_FIND_TIMEOUT);
 
-            if (notFindMaster()) {
-                await this._sendSetMasterEvent();
-                log('syncing: try to set master');
-                await Promise.delay(MASTER_SYNC_TIMEOUT);
-            }
+      if (notFindMaster()) {
+        await this._sendSetMasterEvent();
+        log('syncing: try to set master');
+        await Promise.delay(MASTER_SYNC_TIMEOUT);
+      }
 
-            if (this._isMaster) {
-            log('syncing: I MASTER')
-            }
+      if (this._isMaster) 
+        log('syncing: I MASTER');
+            
 
-            log('syncing: finished')      
-            this._isMasterSynced = true;
-        };
+      log('syncing: finished');      
+      this._isMasterSynced = true;
+    };
         
-    }
+  }
 
-    /**
+  /**
      * 
      * Async start function
      * in this function process subscribe on main events in rabbitmq, connected to elections
@@ -126,16 +125,16 @@ class MasterNode {
      * 
      * @memberOf MasterNode
      */
-    async start() {
-        await this.channel.assertExchange(EXCHANGE_NAME, 'direct', {autoDelete: true});
-        this._onSetMasterEvent(this._setNewMasterFromEvent);
-        this._onFindMasterEvent( 
-          async () => this._isMaster ? await this._sendSetMasterEvent() : ''
-        );
-        setTimeout(this._updateMaster, MASTER_UPDATE_TIMEOUT);
-    }
+  async start () {
+    await this.channel.assertExchange(EXCHANGE_NAME, 'direct', {autoDelete: true});
+    this._onSetMasterEvent(this._setNewMasterFromEvent);
+    this._onFindMasterEvent( 
+      async () => this._isMaster ? await this._sendSetMasterEvent() : ''
+    );
+    setTimeout(this._updateMaster, MASTER_UPDATE_TIMEOUT);
+  }
 
-    /**
+  /**
      * 
      * Async isSyncMaster function
      * check is Master this process, or not
@@ -146,15 +145,15 @@ class MasterNode {
      * 
      * @memberOf MasterNode
      */
-    async isSyncMaster() {
-        if (!this._isMasterSynced) {
-            await Promise.delay(MASTER_SYNC_TIMEOUT);
-        }
-        if (!this._isMaster) {
-          await this._updateMaster();
-        }
-        return this._isMaster;
-    }
+  async isSyncMaster () {
+    if (!this._isMasterSynced) 
+      await Promise.delay(MASTER_SYNC_TIMEOUT);
+        
+    if (!this._isMaster) 
+      await this._updateMaster();
+        
+    return this._isMaster;
+  }
 
 
 }
