15ab25a1231690a700025d936e581bd02d489086
kirya <cloudkserg11@gmail.com>
2018-03-20 17:01:46 +0300

    https://chronobank.atlassian.net/projects/MD/issues/MD-222 - eth middleware - tests for use spawn in discint process testRpc

--- tests/config/index.js
+++ tests/config/index.js
@@ -2,6 +2,7 @@ const config = require('../../config');
 
 config['dev']= {
     uri:  `${/^win/.test(process.platform) ? '\\\\.\\pipe\\' : ''}${`/tmp/${(process.env.NETWORK || 'development')}/geth.ipc`}`,
+    testUri:  `${/^win/.test(process.platform) ? '\\\\.\\pipe\\' : ''}${`/tmp/${(process.env.NETWORK || 'development')}-test/geth.ipc`}`,
     httpUri: 'http://localhost:8545'
 };
 module.exports = config;
