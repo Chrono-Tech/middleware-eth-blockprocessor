508fcb54c3c077765224d5a0aac01ecb9248fa45
zyevegor <zyev.egor@gmail.com>
2018-05-30 09:08:31 +0300

    https://chronobank.atlassian.net/browse/MD-349 - 2fa - replace web3 with ether.js

--- utils/getBlock.js
+++ utils/getBlock.js
@@ -19,23 +19,30 @@ module.exports = async (blockNumber) => {
 
   let rawBlock = await Promise.promisify(web3.eth.getBlock)(blockNumber, true).timeout(10000);
 
-  let logs = await new Promise((res, rej) =>
-    web3.eth.filter({fromBlock: blockNumber, toBlock: blockNumber})
-      .get((err, result) => err ? rej(err) : res(result))
-  ).timeout(30000);
+  if (rawBlock.transactions.length) {
 
-  rawBlock.transactions = rawBlock.transactions.map(tx => {
-    tx.timestamp = rawBlock.timestamp;
-    tx.logs = _.chain(logs)
-      .filter({transactionHash: tx.hash})
-      .map(item => {
-        if (item.topics.length)
-          item.signature = item.topics[0];
-        return item;
-      })
-      .value();
-    return tx;
-  });
+    let logs = await new Promise((res, rej) =>
+      web3.eth.filter({fromBlock: blockNumber, toBlock: blockNumber})
+        .get((err, result) => err ? rej(err) : res(result))
+    ).timeout(30000);
+
+    rawBlock.transactions = rawBlock.transactions.map(tx => {
+      tx.timestamp = rawBlock.timestamp;
+      tx.fee = tx.gas * tx.gasPrice;
+      tx.logs = _.chain(logs)
+        .filter({transactionHash: tx.hash})
+        .map(item => {
+          if (item.topics.length)
+            item.signature = item.topics[0];
+          return item;
+        })
+        .value();
+      return tx;
+    });
+  }
+
+
+  //rawBlock.totalTxFee =
 
   return rawBlock;
 };
